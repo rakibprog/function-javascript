df527451e57a06fb63fd99ea3995ac7e6babce54
rakibprog <contact.engrakib@gmail.com>
2022-05-15 17:36:17 +0600

    update

--- function.js
+++ function.js
@@ -19,3 +19,17 @@ var value1= 10;
 var value2 = 20;
 var result = math(value1,value2);
 console.log(result);
+
+
+// array value add
+
+var numbers = [10,30,40,40,60,80,90,100,120,130]
+
+for(i = 0; i < numbers.length; i++ ){
+        number = numbers[i];
+        console.log(number);
+        if(number > 90){
+            break;
+        }
+       
+}
---
+++ object.js
@@ -0,0 +1,6 @@
+var about = {name: 'rocky', prof:'student', sex:'male'}
+var  name = about.name;
+//set property
+about.prof = 'webdeveloper';
+//get property
+console.log(about); 
